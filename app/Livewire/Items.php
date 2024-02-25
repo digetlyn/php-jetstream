257dbd41a01db03e3682ff591c334cf4eb684a68
dyadya <digetlyn@naver.com>
2024-02-25 20:19:41 +0900

    제트스트림 crud, 목록출력, 페이지네이션

--- app/Livewire/Items.php
+++ app/Livewire/Items.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Item;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Items extends Component
-{
+{   
+    use WithPagination; //페이징 작업..
+
     public function render()
     {
-        return view('livewire.items');
+        $items = Item::where('user_id',auth()->user()->id)->paginate(10);
+        return view('livewire.items', compact('items'));  // compact() 여기에 ['items' => $items];  이렇게 넣어줘도 상관없다.
     }
 }
--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,3 +1,36 @@
-<div>
-    여기는 라이브와이어 컴포넌트에서...
+<div class="p-6 sm:px-20 bg-while border-b border-gary-200">
+    <div class="mt-8 text-2xl">
+    Items
+    </div>  
+    
+
+    <div class="mt-6">
+        <table class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2"><div class="flex items-center">번호</div></th>
+                    <th class="px-4 py-2"><div class="flex items-center">이름</div></th>
+                    <th class="px-4 py-2"><div class="flex items-center">가격</div></th>                
+                    <th class="px-4 py-2">상태</th>
+                    <th class="px-4 py-2">처리</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($items as $item)
+                <tr>
+                    <td class="border px-4 py-2">{{$item->id}}</td>
+                    <td class="border px-4 py-2">{{$item->name}}</td>
+                    <td class="border px-4 py-2">{{number_format($item->price,2)}}</td>
+                    <td class="border px-4 py-2">{{$item->status ? '동작':'동작안함'}}</td>
+                    <td class="border px-4 py-2">수정버튼 삭제버튼</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="mt-4">
+    {{$items->links() }}
+    </div>
+
 </div>
