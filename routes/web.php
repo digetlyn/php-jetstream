4d4c487c72e40840e2c4ee33e49d2e94a74bc3d4
dyadya <digetlyn@naver.com>
2024-02-25 19:47:25 +0900

    제트스트림 CRUD,라이브와이어 컴포넌트 생성

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,12 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/items', function () {  //
+        return view('items');  //블레이드 파일도  items
+    })->name('items');   //라우터 이름도 items
+
+
+
+
 });
